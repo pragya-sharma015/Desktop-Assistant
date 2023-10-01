96517d7c0ca53381e8af41db53b95f14e48befcf
pragya sharma <pragyasharma.techie@gmail.com>
2023-10-01 14:20:36 +0530

    Add Logic for performing task based on wikipedia

--- Desktop-assistant.py
+++ Desktop-assistant.py
@@ -1,4 +1,7 @@
 import pyttsx3
+import speechRecognition as sr
+import wikipedia
+
 
 engine = pyttsx3.init('sapi5')
 voices= engine.getProperty('voices') 
@@ -22,4 +25,43 @@ else:
     speak("Good Evening!!")
 
 Speak("I am your personal assistant Mam , please tell me how may I help you")
- 
+
+# function foe take command
+
+def takeCommand():        #It takes microphone input from the user and returns string output
+   r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+        #for exception
+  try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print(f"User said: {query}\n")  #User query will be printed.
+
+    except Exception as e:
+         print("Say that again please...")  
+        return "None" 
+    return query
+
+    wishme()
+
+     # Logic for executing tasks based on query
+
+
+    while True:
+  
+        query = takeCommand().lower() #Converting user query into lower case
+
+        git # For wikipedia
+
+        if 'wikipedia' in query:  
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2) 
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
